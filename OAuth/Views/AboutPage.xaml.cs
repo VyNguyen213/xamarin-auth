59c2ab452312b26a0b35a9a2afc1f1c8536cde99
VyNguyen213 <nhavy.nguyen213@gmail.com>
2019-02-04 22:19:10 +0700

    init

---
+++ OAuth/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OAuth.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
